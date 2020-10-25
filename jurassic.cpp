bcf2d83e8826ba22fa4654ae9ddb8cfd55ace31b
Nagatyy <mohamednagaty@aucegypt.edu>
2020-10-25 22:14:28 +0200

    Commit1

--- jurassic.cpp
+++ jurassic.cpp
@@ -1,8 +1,78 @@
+#include <QThread>
+#include <QMutex>
+#include <QSemaphore>
 #include <iostream>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char const *argv[]){
+using namespace std;
 
-	std::cout << "Hello World" << std::endl;
-	
-	return 0;
+QSemaphore carsAvailable;
+QSemaphore carsTaken;
+
+
+
+class Car : public QThread{
+private:
+    int ID;
+public:
+    Car(int ID){     
+        this -> ID = ID + 1;
+
+
+    }
+    void run(){
+           
+    }
+};
+
+
+class Visitor : public QThread{
+private:
+    int ID;
+    int waitingTime;
+public:
+    Visitor(int ID, waitingTime){
+    	this -> ID = ID;
+    	cout << "Visitor " << ID << " Entered the park\n";
+
+    	
+    } 
+    void run(){
+    	sleep((long) waitingTime);
+    	cout << "Visitor " << ID << " slept for " << waitingTime << " seconds\n";
+ 
+    }
+};
+
+//================================================
+int main(int argc, char** argv){
+
+    srand(time(NULL)^getpid());
+
+     if (argc != 3){
+        std::cout << "Incorrect Number of Arguments!" <<std::endl;
+        return 0; 
+    }
+    
+    int N = atoi(argv[1]);					// number of visitors
+    int M = atoi(argv[2]);                 	// number of cars
+
+    Visitor* v[N];
+    Car* c[M];
+    int waitingTime;
+
+
+    carsAvailable = new QSemaphore(M);
+    carsTaken = new QSemaphore(0);
+
+    for(int i = 0; i < N; i++){
+    	waitingTime = rand() % 5;    	
+    	v[i] = new Visitor(i+1, waitingTime);
+
+    }
+
+  
+    return 0;
 }
